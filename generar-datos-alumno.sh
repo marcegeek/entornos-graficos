176600a1c9314eabfe1b01ec04a22468b4e369b8
Marcelo Castellano <marce.geek22@gmail.com>
2017-05-15 20:16:52 -0300

    mejoras en scripts

--- generar-datos-alumno.sh
+++ generar-datos-alumno.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-if [[ "$#" -ne "3" ]]; then
+if [ "$#" -ne "3" ]; then
    echo Uso: generar-datos-alumno.sh nombre_apellido legajo email
    exit 1
 fi
@@ -9,4 +9,6 @@ nombre=$1
 legajo=$2
 email=$3
 
-echo "Nombre y apellido: $nombre<br>\nLegajo: $legajo<br>\nE-mail: $email"
+(echo nombre:$nombre
+echo legajo:$legajo
+echo email:$email) > datos-alumno
--- generar-trabajo.sh
+++ generar-trabajo.sh
@@ -1,11 +1,18 @@
 #!/bin/sh
 
-if [[ "$#" -ne "3" ]]; then
+if [ "$#" -ne "3" ] && [ ! -f "datos-alumno" ]; then
    echo Uso: generar-trabajo.sh nombre_apellido legajo email
    exit 1
 fi
 
-datos_alumno=$(./generar-datos-alumno.sh "$1" "$2" "$3")
+if [ ! -f "datos-alumno" ]; then
+   $(./generar-datos-alumno.sh "$1" "$2" "$3")
+fi
+datos_alumno=$(sed -e 's/nombre:/Nombre y apellido: /'\
+  -e 's/legajo:/Legajo: /'\
+  -e 's/email:/E-mail: /'\
+  -e "s/$/<br>\\\\n/g" datos-alumno | tr -d '\n')
+
 indice=$(./generar-indice.sh)
 
 sed -e "s/{student-data}/$datos_alumno/" -e "s/{generated-index}/$indice/" < trabajo.md > trabajo-generated.md
